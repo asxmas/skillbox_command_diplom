b9146770861e759ab219dc6c2631d6116750fd08
pavel-mets <pavel.mets@yahoo.com>
2021-06-05 11:34:58 +0300

    application.yml correction

--- src/main/java/ru/skillbox/team13/service/impl/UserServiceImpl.java
+++ src/main/java/ru/skillbox/team13/service/impl/UserServiceImpl.java
@@ -63,13 +63,13 @@ public class UserServiceImpl implements UserService {
             return null;
         }
 
-        log.info("IN findById - user: {} found by id: {}", result);
+        log.info("IN findById - user found by id: {}", result);
         return result;
     }
 
     @Override
     public void delete(Long id) {
         userRepository.deleteById(id);
-        log.info("IN delete - user with id: {} successfully deleted");
+        log.info("IN delete - user with id: {} successfully deleted", id);
     }
 }
