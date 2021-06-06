98a605ce9064d500af0d0079b1a936fc212baebe
levchigintsev <lev.chigintsev@gmail.com>
2021-06-06 19:10:46 +0300

    basic auth implementation, with noop pass encoder

---
+++ src/main/java/ru/skillbox/team13/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ru.skillbox.team13.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.skillbox.team13.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByName(String name);
+}
