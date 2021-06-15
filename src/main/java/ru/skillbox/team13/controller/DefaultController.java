c7cacd4abc253341e8941015a0c555debb766d14
levchigintsev <lev.chigintsev@gmail.com>
2021-06-15 18:59:11 +0300

    pom, config and static res fixes

--- src/main/java/ru/skillbox/team13/controller/DefaultController.java
+++ src/main/java/ru/skillbox/team13/controller/DefaultController.java
@@ -1,15 +1,14 @@
 package ru.skillbox.team13.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultController {
 
     @GetMapping("/")
-    public String index(Model model){
-        return "index";
+    public String index(){
+        return "static/index.html";
     }
 
 }
