a0a9e4f0d328d27f9c8a3d8bf3590b95b2d64387
MikeDuz <mike1978@list.ru>
2021-06-30 16:19:50 +0300

    Post start

---
+++ src/main/java/ru/skillbox/team13/dto/PostCommentDto.java
@@ -0,0 +1,20 @@
+package ru.skillbox.team13.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PostCommentDto {
+
+    @JsonProperty("parent_id")
+    int parentId;
+    @JsonProperty("comment_text")
+    String commentText;
+    int id;
+    @JsonProperty("post_id")
+    int postId;
+    long time;
+    @JsonProperty("author_id")
+    int authorId;
+    @JsonProperty("is_blocked")
+    boolean isBlocked;
+
+}
