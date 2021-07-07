e4e8145498519412ddf8665786a5a82f88ff53bb
levchigintsev <lev.chigintsev@gmail.com>
2021-07-07 11:24:48 +0300

    friendship columns null fix

--- src/main/resources/db/changelogs/10-friendship-fix/10-2-create-columns-in-friendship.sql
+++ src/main/resources/db/changelogs/10-friendship-fix/10-2-create-columns-in-friendship.sql
@@ -2,10 +2,10 @@
 
 -- changeset lc:10-2-add-new-columns-to-friendship
 ALTER TABLE friendship
-    ADD "code" VARCHAR(50) NOT NULL ;
+    ADD "code" VARCHAR(50) NOT NULL default 'REQUEST' ;
 
 ALTER TABLE friendship
-    ADD "name" VARCHAR(255) NOT NULL;
+    ADD "name" VARCHAR(255) NOT NULL default 'Friendship is magic!';
 
 ALTER TABLE friendship
-    ADD "time" TIMESTAMP WITHOUT TIME ZONE NOT NULL;
+    ADD "time" TIMESTAMP WITHOUT TIME ZONE NOT NULL default now();
