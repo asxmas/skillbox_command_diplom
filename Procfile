9dd037fc005504b7056cd3da3e6e92b98528952e
pavel-mets <pavel.mets@yahoo.com>
2021-07-08 14:27:55 +0300

    team13

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/socnet-1.0.jar
--- src/main/java/ru/skillbox/team13/config/SecurityConfig.java
+++ src/main/java/ru/skillbox/team13/config/SecurityConfig.java
@@ -11,27 +11,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.skillbox.team13.security.Jwt.JwtConfigurer;
 import ru.skillbox.team13.security.Jwt.JwtTokenProvider;
-
-import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
-private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Value("${server.base_url}")
     private String BASE_URL;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
                 .httpBasic().disable()
                 .csrf().disable()
@@ -41,20 +37,14 @@ private final JwtTokenProvider jwtTokenProvider;
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .cors()
-                .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(BASE_URL));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(BASE_URL)
+                .allowedMethods("*");
     }
 
     @Override
